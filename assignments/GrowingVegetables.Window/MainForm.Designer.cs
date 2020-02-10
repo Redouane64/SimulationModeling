350dec273b825e14e281f566d025bddfba0ea7e2
Redhouane Sobaihi <s-redouane@outlook.com>
2020-02-10 16:34:38 +0700

    Create main window.

---
+++ assignments/GrowingVegetables.Window/MainForm.Designer.cs
@@ -0,0 +1,358 @@
+﻿namespace GrowingVegetables.Window
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.FourByFourFarm = new System.Windows.Forms.TableLayoutPanel();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox6 = new System.Windows.Forms.CheckBox();
+            this.checkBox7 = new System.Windows.Forms.CheckBox();
+            this.checkBox8 = new System.Windows.Forms.CheckBox();
+            this.checkBox9 = new System.Windows.Forms.CheckBox();
+            this.checkBox10 = new System.Windows.Forms.CheckBox();
+            this.checkBox11 = new System.Windows.Forms.CheckBox();
+            this.checkBox12 = new System.Windows.Forms.CheckBox();
+            this.checkBox13 = new System.Windows.Forms.CheckBox();
+            this.checkBox14 = new System.Windows.Forms.CheckBox();
+            this.checkBox15 = new System.Windows.Forms.CheckBox();
+            this.checkBox16 = new System.Windows.Forms.CheckBox();
+            this.Container = new System.Windows.Forms.SplitContainer();
+            this.mainMenuStrip.SuspendLayout();
+            this.FourByFourFarm.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Container)).BeginInit();
+            this.Container.Panel2.SuspendLayout();
+            this.Container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(384, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.Exit);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(3, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox1.Size = new System.Drawing.Size(63, 62);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // FourByFourFarm
+            // 
+            this.FourByFourFarm.ColumnCount = 4;
+            this.FourByFourFarm.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.FourByFourFarm.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.FourByFourFarm.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.FourByFourFarm.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.FourByFourFarm.Controls.Add(this.checkBox16, 3, 3);
+            this.FourByFourFarm.Controls.Add(this.checkBox15, 2, 3);
+            this.FourByFourFarm.Controls.Add(this.checkBox14, 1, 3);
+            this.FourByFourFarm.Controls.Add(this.checkBox13, 0, 3);
+            this.FourByFourFarm.Controls.Add(this.checkBox12, 3, 2);
+            this.FourByFourFarm.Controls.Add(this.checkBox11, 2, 2);
+            this.FourByFourFarm.Controls.Add(this.checkBox10, 1, 2);
+            this.FourByFourFarm.Controls.Add(this.checkBox9, 0, 2);
+            this.FourByFourFarm.Controls.Add(this.checkBox8, 3, 1);
+            this.FourByFourFarm.Controls.Add(this.checkBox7, 2, 1);
+            this.FourByFourFarm.Controls.Add(this.checkBox6, 1, 1);
+            this.FourByFourFarm.Controls.Add(this.checkBox5, 0, 1);
+            this.FourByFourFarm.Controls.Add(this.checkBox4, 3, 0);
+            this.FourByFourFarm.Controls.Add(this.checkBox3, 2, 0);
+            this.FourByFourFarm.Controls.Add(this.checkBox2, 1, 0);
+            this.FourByFourFarm.Controls.Add(this.checkBox1, 0, 0);
+            this.FourByFourFarm.Location = new System.Drawing.Point(52, 26);
+            this.FourByFourFarm.Name = "FourByFourFarm";
+            this.FourByFourFarm.RowCount = 4;
+            this.FourByFourFarm.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.FourByFourFarm.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.FourByFourFarm.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.FourByFourFarm.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.FourByFourFarm.Size = new System.Drawing.Size(278, 273);
+            this.FourByFourFarm.TabIndex = 2;
+            this.FourByFourFarm.Click += new System.EventHandler(this.CheckBox_Clicked);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(72, 3);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox2.Size = new System.Drawing.Size(63, 62);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.Color.White;
+            this.checkBox3.Location = new System.Drawing.Point(141, 3);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox3.Size = new System.Drawing.Size(63, 62);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.UseVisualStyleBackColor = false;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.BackColor = System.Drawing.Color.White;
+            this.checkBox4.Location = new System.Drawing.Point(210, 3);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox4.Size = new System.Drawing.Size(63, 62);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.UseVisualStyleBackColor = false;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.BackColor = System.Drawing.Color.White;
+            this.checkBox5.Location = new System.Drawing.Point(3, 71);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox5.Size = new System.Drawing.Size(63, 62);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.UseVisualStyleBackColor = false;
+            // 
+            // checkBox6
+            // 
+            this.checkBox6.AutoSize = true;
+            this.checkBox6.BackColor = System.Drawing.Color.White;
+            this.checkBox6.Location = new System.Drawing.Point(72, 71);
+            this.checkBox6.Name = "checkBox6";
+            this.checkBox6.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox6.Size = new System.Drawing.Size(63, 62);
+            this.checkBox6.TabIndex = 6;
+            this.checkBox6.UseVisualStyleBackColor = false;
+            // 
+            // checkBox7
+            // 
+            this.checkBox7.AutoSize = true;
+            this.checkBox7.BackColor = System.Drawing.Color.White;
+            this.checkBox7.Location = new System.Drawing.Point(141, 71);
+            this.checkBox7.Name = "checkBox7";
+            this.checkBox7.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox7.Size = new System.Drawing.Size(63, 62);
+            this.checkBox7.TabIndex = 7;
+            this.checkBox7.UseVisualStyleBackColor = false;
+            // 
+            // checkBox8
+            // 
+            this.checkBox8.AutoSize = true;
+            this.checkBox8.BackColor = System.Drawing.Color.White;
+            this.checkBox8.Location = new System.Drawing.Point(210, 71);
+            this.checkBox8.Name = "checkBox8";
+            this.checkBox8.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox8.Size = new System.Drawing.Size(63, 62);
+            this.checkBox8.TabIndex = 8;
+            this.checkBox8.UseVisualStyleBackColor = false;
+            // 
+            // checkBox9
+            // 
+            this.checkBox9.AutoSize = true;
+            this.checkBox9.BackColor = System.Drawing.Color.White;
+            this.checkBox9.Location = new System.Drawing.Point(3, 139);
+            this.checkBox9.Name = "checkBox9";
+            this.checkBox9.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox9.Size = new System.Drawing.Size(63, 62);
+            this.checkBox9.TabIndex = 9;
+            this.checkBox9.UseVisualStyleBackColor = false;
+            // 
+            // checkBox10
+            // 
+            this.checkBox10.AutoSize = true;
+            this.checkBox10.BackColor = System.Drawing.Color.White;
+            this.checkBox10.Location = new System.Drawing.Point(72, 139);
+            this.checkBox10.Name = "checkBox10";
+            this.checkBox10.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox10.Size = new System.Drawing.Size(63, 62);
+            this.checkBox10.TabIndex = 10;
+            this.checkBox10.UseVisualStyleBackColor = false;
+            // 
+            // checkBox11
+            // 
+            this.checkBox11.AutoSize = true;
+            this.checkBox11.BackColor = System.Drawing.Color.White;
+            this.checkBox11.Location = new System.Drawing.Point(141, 139);
+            this.checkBox11.Name = "checkBox11";
+            this.checkBox11.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox11.Size = new System.Drawing.Size(63, 62);
+            this.checkBox11.TabIndex = 11;
+            this.checkBox11.UseVisualStyleBackColor = false;
+            // 
+            // checkBox12
+            // 
+            this.checkBox12.AutoSize = true;
+            this.checkBox12.BackColor = System.Drawing.Color.White;
+            this.checkBox12.Location = new System.Drawing.Point(210, 139);
+            this.checkBox12.Name = "checkBox12";
+            this.checkBox12.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox12.Size = new System.Drawing.Size(63, 62);
+            this.checkBox12.TabIndex = 12;
+            this.checkBox12.UseVisualStyleBackColor = false;
+            // 
+            // checkBox13
+            // 
+            this.checkBox13.AutoSize = true;
+            this.checkBox13.BackColor = System.Drawing.Color.White;
+            this.checkBox13.Location = new System.Drawing.Point(3, 207);
+            this.checkBox13.Name = "checkBox13";
+            this.checkBox13.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox13.Size = new System.Drawing.Size(63, 62);
+            this.checkBox13.TabIndex = 13;
+            this.checkBox13.UseVisualStyleBackColor = false;
+            // 
+            // checkBox14
+            // 
+            this.checkBox14.AutoSize = true;
+            this.checkBox14.BackColor = System.Drawing.Color.White;
+            this.checkBox14.Location = new System.Drawing.Point(72, 207);
+            this.checkBox14.Name = "checkBox14";
+            this.checkBox14.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox14.Size = new System.Drawing.Size(63, 62);
+            this.checkBox14.TabIndex = 14;
+            this.checkBox14.UseVisualStyleBackColor = false;
+            // 
+            // checkBox15
+            // 
+            this.checkBox15.AutoSize = true;
+            this.checkBox15.BackColor = System.Drawing.Color.White;
+            this.checkBox15.Location = new System.Drawing.Point(141, 207);
+            this.checkBox15.Name = "checkBox15";
+            this.checkBox15.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox15.Size = new System.Drawing.Size(63, 62);
+            this.checkBox15.TabIndex = 15;
+            this.checkBox15.UseVisualStyleBackColor = false;
+            // 
+            // checkBox16
+            // 
+            this.checkBox16.AutoSize = true;
+            this.checkBox16.BackColor = System.Drawing.Color.White;
+            this.checkBox16.Location = new System.Drawing.Point(210, 207);
+            this.checkBox16.Name = "checkBox16";
+            this.checkBox16.Padding = new System.Windows.Forms.Padding(24);
+            this.checkBox16.Size = new System.Drawing.Size(63, 62);
+            this.checkBox16.TabIndex = 16;
+            this.checkBox16.UseVisualStyleBackColor = false;
+            // 
+            // Container
+            // 
+            this.Container.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Container.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Container.Location = new System.Drawing.Point(0, 24);
+            this.Container.Name = "Container";
+            this.Container.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // Container.Panel2
+            // 
+            this.Container.Panel2.Controls.Add(this.FourByFourFarm);
+            this.Container.Size = new System.Drawing.Size(384, 407);
+            this.Container.SplitterDistance = 81;
+            this.Container.SplitterWidth = 1;
+            this.Container.TabIndex = 4;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 431);
+            this.Controls.Add(this.Container);
+            this.Controls.Add(this.mainMenuStrip);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "Growing Vegetables Simulation";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.FourByFourFarm.ResumeLayout(false);
+            this.FourByFourFarm.PerformLayout();
+            this.Container.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Container)).EndInit();
+            this.Container.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TableLayoutPanel FourByFourFarm;
+        private System.Windows.Forms.CheckBox checkBox16;
+        private System.Windows.Forms.CheckBox checkBox15;
+        private System.Windows.Forms.CheckBox checkBox14;
+        private System.Windows.Forms.CheckBox checkBox13;
+        private System.Windows.Forms.CheckBox checkBox12;
+        private System.Windows.Forms.CheckBox checkBox11;
+        private System.Windows.Forms.CheckBox checkBox10;
+        private System.Windows.Forms.CheckBox checkBox9;
+        private System.Windows.Forms.CheckBox checkBox8;
+        private System.Windows.Forms.CheckBox checkBox7;
+        private System.Windows.Forms.CheckBox checkBox6;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.SplitContainer Container;
+    }
+}
+
---
+++ assignments/GrowingVegetables.Window/MainForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GrowingVegetables.Window
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Attch click handler to CheckBox controls
+            foreach(var checkbox in FourByFourFarm.Controls.OfType<CheckBox>())
+            {
+                checkbox.Click += new EventHandler(CheckBox_Clicked);
+            }
+        }
+
+        private void CheckBox_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Exit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
