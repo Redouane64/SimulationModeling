1d13899209a5455e1c1f636d85b71857abd47657
Redhouane Sobaihi <s-redouane@outlook.com>
2020-03-07 12:09:34 +0700

    Flight without atmosphere

---
+++ assignments/Flight/MainForm.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Flight
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            System.Windows.Forms.Label label3;
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.flightChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.simulationTime = new System.Windows.Forms.Timer(this.components);
+            this.heightNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.angelNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.speedNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.launchButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.timeTextBox = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.flightChart)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angelNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedNumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(13, 28);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(38, 13);
+            label1.TabIndex = 1;
+            label1.Text = "Height";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(17, 64);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(34, 13);
+            label2.TabIndex = 2;
+            label2.Text = "Angle";
+            label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(12, 98);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(38, 13);
+            label3.TabIndex = 3;
+            label3.Text = "Speed";
+            label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // flightChart
+            // 
+            chartArea1.AxisX.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisX.ScaleBreakStyle.StartFromZero = System.Windows.Forms.DataVisualization.Charting.StartFromZero.Yes;
+            chartArea1.AxisY.IntervalAutoMode = System.Windows.Forms.DataVisualization.Charting.IntervalAutoMode.VariableCount;
+            chartArea1.AxisY.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.AxisY.Minimum = 0D;
+            chartArea1.AxisY.ScaleBreakStyle.StartFromZero = System.Windows.Forms.DataVisualization.Charting.StartFromZero.Yes;
+            chartArea1.Name = "FlightChartArea";
+            this.flightChart.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.flightChart.Legends.Add(legend1);
+            this.flightChart.Location = new System.Drawing.Point(12, 162);
+            this.flightChart.Name = "flightChart";
+            series1.ChartArea = "FlightChartArea";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Flight";
+            series1.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            series1.YValuesPerPoint = 2;
+            series1.YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+            this.flightChart.Series.Add(series1);
+            this.flightChart.Size = new System.Drawing.Size(600, 400);
+            this.flightChart.TabIndex = 0;
+            this.flightChart.Text = "Flight";
+            // 
+            // simulationTime
+            // 
+            this.simulationTime.Tick += new System.EventHandler(this.SimulationTime_Tick);
+            // 
+            // heightNumericUpDown
+            // 
+            this.heightNumericUpDown.Location = new System.Drawing.Point(57, 26);
+            this.heightNumericUpDown.Name = "heightNumericUpDown";
+            this.heightNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.heightNumericUpDown.TabIndex = 4;
+            // 
+            // angelNumericUpDown
+            // 
+            this.angelNumericUpDown.Location = new System.Drawing.Point(57, 62);
+            this.angelNumericUpDown.Name = "angelNumericUpDown";
+            this.angelNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.angelNumericUpDown.TabIndex = 5;
+            this.angelNumericUpDown.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // speedNumericUpDown
+            // 
+            this.speedNumericUpDown.Location = new System.Drawing.Point(57, 96);
+            this.speedNumericUpDown.Name = "speedNumericUpDown";
+            this.speedNumericUpDown.Size = new System.Drawing.Size(120, 20);
+            this.speedNumericUpDown.TabIndex = 6;
+            this.speedNumericUpDown.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // launchButton
+            // 
+            this.launchButton.Location = new System.Drawing.Point(223, 83);
+            this.launchButton.Name = "launchButton";
+            this.launchButton.Size = new System.Drawing.Size(75, 23);
+            this.launchButton.TabIndex = 7;
+            this.launchButton.Text = "Launch";
+            this.launchButton.UseVisualStyleBackColor = true;
+            this.launchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.timeTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(183, 21);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 56);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Time";
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Location = new System.Drawing.Point(304, 83);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(75, 23);
+            this.pauseButton.TabIndex = 9;
+            this.pauseButton.Text = "Pause";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.PauseButton_Click);
+            // 
+            // timeTextBox
+            // 
+            this.timeTextBox.Location = new System.Drawing.Point(7, 20);
+            this.timeTextBox.Name = "timeTextBox";
+            this.timeTextBox.ReadOnly = true;
+            this.timeTextBox.Size = new System.Drawing.Size(189, 20);
+            this.timeTextBox.TabIndex = 0;
+            this.timeTextBox.Text = "0";
+            this.timeTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(625, 574);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.launchButton);
+            this.Controls.Add(this.speedNumericUpDown);
+            this.Controls.Add(this.angelNumericUpDown);
+            this.Controls.Add(this.heightNumericUpDown);
+            this.Controls.Add(label3);
+            this.Controls.Add(label2);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.flightChart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Flight (Without Atmosphere)";
+            ((System.ComponentModel.ISupportInitialize)(this.flightChart)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.heightNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.angelNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedNumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart flightChart;
+        private System.Windows.Forms.Timer simulationTime;
+        private System.Windows.Forms.NumericUpDown heightNumericUpDown;
+        private System.Windows.Forms.NumericUpDown angelNumericUpDown;
+        private System.Windows.Forms.NumericUpDown speedNumericUpDown;
+        private System.Windows.Forms.Button launchButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox timeTextBox;
+        private System.Windows.Forms.Button pauseButton;
+    }
+}
+
---
+++ assignments/Flight/MainForm.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Flight
+{
+    public partial class MainForm : Form
+    {
+        private readonly Series series;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Setup.
+            series = flightChart.Series[0];
+        }
+
+        private decimal t, x0, y0, v0, cosa, sina;
+        private bool isRunning = false;
+
+        private void PauseButton_Click(object sender, EventArgs e)
+        {
+            pauseButton.Text = (isRunning = !isRunning) ? "Pause" : "Resume";
+        }
+
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            if (!simulationTime.Enabled)
+            {
+                series.Points.Clear();
+                t = 0;
+                x0 = 0;
+                y0 = heightNumericUpDown.Value;
+                v0 = speedNumericUpDown.Value;
+
+                double a = (double)angelNumericUpDown.Value * Math.PI / 100;
+                cosa = (decimal)Math.Cos(a);
+                sina = (decimal)Math.Sin(a);
+
+                series.Points.AddXY(x0, y0);
+                simulationTime.Start();
+
+                isRunning = true;
+                launchButton.Enabled = false;
+            }
+        }
+
+        const decimal g = 9.81M;
+        const decimal dt = 0.1M;
+
+        private void SimulationTime_Tick(object sender, EventArgs e)
+        {
+            if(!isRunning)
+            {
+                return;
+            }
+
+            t += dt;
+
+            timeTextBox.Text = t.ToString("f2");
+
+            decimal x = x0 + v0 * cosa * t;
+            decimal y = y0 + v0 * sina * t - g * t * t / 2;
+            series.Points.AddXY(x, y);
+
+            if (y <= 0)
+            {
+                simulationTime.Stop();
+                isRunning = false;
+                launchButton.Enabled = true;
+            }
+        }
+
+    }
+}
