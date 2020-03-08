76174875a968a40ee852a8fe3958a9f86426d8b9
Redhouane Sobaihi <s-redouane@outlook.com>
2020-03-08 17:58:27 +0700

    Currency exchange

---
+++ assignments/CurrencyExchange/MainForm.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace CurrencyExchange
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.initialPriceInput = new System.Windows.Forms.NumericUpDown();
+            this.daysInput = new System.Windows.Forms.NumericUpDown();
+            this.calculateButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initialPriceInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daysInput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisX.Minimum = 0D;
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 73);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            series1.IsValueShownAsLabel = true;
+            series1.IsVisibleInLegend = false;
+            series1.LabelFormat = "f4";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(584, 395);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Initial Price";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Days";
+            // 
+            // initialPriceInput
+            // 
+            this.initialPriceInput.DecimalPlaces = 4;
+            this.initialPriceInput.Location = new System.Drawing.Point(77, 18);
+            this.initialPriceInput.Name = "initialPriceInput";
+            this.initialPriceInput.Size = new System.Drawing.Size(120, 20);
+            this.initialPriceInput.TabIndex = 3;
+            this.initialPriceInput.ThousandsSeparator = true;
+            this.initialPriceInput.Value = new decimal(new int[] {
+            73,
+            0,
+            0,
+            0});
+            // 
+            // daysInput
+            // 
+            this.daysInput.Location = new System.Drawing.Point(77, 47);
+            this.daysInput.Name = "daysInput";
+            this.daysInput.Size = new System.Drawing.Size(120, 20);
+            this.daysInput.TabIndex = 4;
+            this.daysInput.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(204, 18);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(89, 49);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 480);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.daysInput);
+            this.Controls.Add(this.initialPriceInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.initialPriceInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.daysInput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown initialPriceInput;
+        private System.Windows.Forms.NumericUpDown daysInput;
+        private System.Windows.Forms.Button calculateButton;
+    }
+}
+
---
+++ assignments/CurrencyExchange/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyExchange
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, initialPriceInput.Value);
+
+            var calculator = new CurrencyExchangeCalculator((double)initialPriceInput.Value, (int)daysInput.Value);
+
+            foreach (var value in calculator.Start())
+            {
+                chart1.Series[0].Points.AddXY(value.day, value.price);
+            }
+        }
+    }
+
+    public class CurrencyExchangeCalculator
+    {
+
+        const double k = 0.02;
+
+        private readonly Random rnd = new Random();
+
+        private double price;
+        private readonly int days;
+
+        public CurrencyExchangeCalculator(double initialPrice, int days)
+        {
+            this.price = initialPrice;
+            this.days = days;
+        }
+
+        public IEnumerable<(int day, double price)> Start()
+        {
+            for (int i = 1; i <= this.days; i++)
+            {
+                price = price * (1 + k * (rnd.NextDouble() - 0.5));
+                yield return (i, price);
+            }
+        }
+    }
+}
